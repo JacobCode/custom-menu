9d27717be41e51a77e1177a90abb9f5980999625
Jacob Carver <jacxbcarver@gmail.com>
2018-10-26 06:37:51 -0600

    added more animations

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    // open and close menu
     $('#arrow').on('click', function () {
         $('#options').toggleClass('active');
         $('#arrow').toggleClass('active');
@@ -12,27 +13,42 @@ $(document).ready(function() {
         $(this).children($('.change-class')).removeClass('active');
     });
 
+    // clicking on new category
     $('.option').on('click', function () {
+        var currentOption = $(this);
+
         var previousCategory = $('#current-category')[0];
-        var currentIcon = $('#current-icon').children('i')[0];
+        var previousIcon = $('#current-icon').children('i')[0];
 
-        var top = $('.left .change-icon');
-        top.empty();
+        var newCategory = currentOption.children('.category')[0];
+        var newIcon = currentOption.children()[1].children[0];
 
-        var newCategory = $(this).children('.category')[0];
-        var newIcon = $(this).children()[1].children[0];
-
-        $(this).children('.change-icon').append(currentIcon);
+        animateIcon(newIcon);
 
         var previous = previousCategory.innerHTML;
 
-        update(top, newIcon, previousCategory, newCategory);
+        setTimeout(function () {
+            var top = $('.left .change-icon');
+            top.empty();
 
-        $(this).children('.category')[0].innerHTML = previous;
+            currentOption.children('.change-icon').append(previousIcon);
+
+            update(top, newIcon, previousCategory, newCategory);
+
+            currentOption.children('.category')[0].innerHTML = previous;
+        }, 1000);
     });
     
     function update(top, newIcon, previousCategory, newCategory) {
         previousCategory.innerHTML = newCategory.innerHTML;
         top.append(newIcon);
+        newIcon.style.transform = 'initial';
+    }
+
+    function animateIcon(newIcon) {
+        newIcon.style.transform = 'rotate(360deg)';
+        setTimeout(function () {
+            newIcon.style.transform = 'translate3d(250%, 0, 0)';
+        }, 500);
     }
 })
