8bbf6b5b0922cd9e65dfb5e79a4b1d370ec44101
Jacob Carver <jacxbcarver@gmail.com>
2018-10-28 17:51:27 -0600

    update

--- docs/app.js
+++ docs/app.js
@@ -15,13 +15,18 @@ $(document).ready(function() {
 
     $('.option').on('mouseover', function () {
         $(this).children($('.change-class')).addClass('active');
+        for (var i = 0; i < $(this).length; i++) {
+            $(this)[i].style.borderWidth = '5px';
+        }
     });
 
     $('.option').on('mouseout', function () {
         $(this).children($('.change-class')).removeClass('active');
-        for (var i = 0; i < $('.option .change-icon').length; i++) {
+        for (var i = 0; i < $('.option').length; i++) {
             $('.option .change-icon')[i].style.transform = 'translate3d(250%, 0, 0)';
+            $('.option')[i].style.borderWidth = '0px';
         }
+        $(this).removeClass('item');
     });
     
     // clicking on new category
@@ -32,14 +37,20 @@ $(document).ready(function() {
         var newCategory = currentOption.children('.category')[0].firstChild;
         var newIcon = currentOption.children()[1].children[0];
 
-        $(this).addClass('item');
-        $('#options .item')[0].lastElementChild.style.transform = 'translate3d(0, 0, 0) !important';
-        console.log($('#options .item'));
+        var click = $(this);
+        
+        var previousColor = $(previousIcon).css('color')
 
-        animateIcon(newIcon);
+        $(this).addClass('item');
+
+        newIcon.style.transform = 'rotate(360deg)';
+
+        animateIcon();
+
         setTimeout(function () {
             previousCategory.style.opacity = 0;
             newCategory.style.opacity = 0;
+            click[0].style.borderWidth = '0px';
         }, 500);
 
         setTimeout(function () {
@@ -60,7 +71,10 @@ $(document).ready(function() {
             setTimeout(function () {
                 previousCategory.style.opacity = 1;
                 newCategory.style.opacity = 1;
-            }, 200);
+            }, 250);
+            click[0].style.borderColor = previousColor;
+            click[0].style.opacity = '1';
+            click[0].style.borderWidth = '5px';
         }, 1000);
     });
 
@@ -69,11 +83,13 @@ $(document).ready(function() {
         previousCategory.innerHTML = newCategory.innerHTML;
     }
 
-    function animateIcon(newIcon) {
-        newIcon.style.transform = 'rotate(360deg)';
+    function animateIcon() {
         setTimeout(function () {
-            for (var i = 0; i < $('.option .change-icon').length; i++) {
+            for (var i = 0; i < $('.option').length; i++) {
                 $('.option .change-icon')[i].style.transform = 'translate3d(250%, 0, 0)';
+                setTimeout(function () {
+                    $('#options .item')[0].lastElementChild.style.transform = 'translate3d(0, 0, 0)';
+                }, 500)
             }
         }, 500);
     }
