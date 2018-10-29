52b40d32b5847c107d87fbf5a2bc9cb1bea3ab4a
Jacob Carver <jacxbcarver@gmail.com>
2018-10-29 10:19:12 -0600

    added comments and cleaned up

--- docs/app.js
+++ docs/app.js
@@ -1,25 +1,22 @@
 $(document).ready(function() {
+    // move all icons in options list to the right by default
     for (var i = 0; i < $('.option .change-icon').length; i++) {
         $('.option .change-icon')[i].style.transform = 'translate3d(250%, 0, 0)';
     }
-
-    $('.option').on('mouseover', function() {
-        $(this)[0].children[1].style.transform = 'translate3d(0, 0, 0)';
-    });
-
     // open and close menu
-    $('#arrow').on('click', function () {
+    $('.select').on('click', function () {
         $('#options').toggleClass('active');
         $('#arrow').toggleClass('active');
     });
-
+    // hover option
     $('.option').on('mouseover', function () {
+        $(this)[0].children[1].style.transform = 'translate3d(0, 0, 0)';
         $(this).children($('.change-class')).addClass('active');
         for (var i = 0; i < $(this).length; i++) {
-            $(this)[i].style.borderWidth = '5px';
+            $(this)[i].style.borderWidth = '10px';
         }
     });
-
+    // leaving option
     $('.option').on('mouseout', function () {
         $(this).children($('.change-class')).removeClass('active');
         for (var i = 0; i < $('.option').length; i++) {
@@ -28,74 +25,90 @@ $(document).ready(function() {
         }
         $(this).removeClass('item');
     });
-    
-    // clicking on new category
+    // clicking on an new option
     $('.option').on('click', function () {
         var currentOption = $(this);
         var previousCategory = $('#current-category')[0];
         var previousIcon = $('#current-icon').children('i')[0];
         var newCategory = currentOption.children('.category')[0].firstChild;
         var newIcon = currentOption.children()[1].children[0];
-
-        var click = $(this);
-        
         var previousColor = $(previousIcon).css('color')
 
+        // add item class
         $(this).addClass('item');
 
-        newIcon.style.transform = 'rotate(360deg)';
-
+        // call animateIcon function
         animateIcon();
 
         setTimeout(function () {
+            // change opacity of previous category (hide)
             previousCategory.style.opacity = 0;
+            // change opacity of new category (hide)
             newCategory.style.opacity = 0;
-            click[0].style.borderWidth = '0px';
+            // change opacity of option (hide)
+            currentOption[0].style.opacity = '0';
+            // change left borderWidth of option (hide)
+            currentOption[0].style.borderWidth = '0px';
         }, 500);
 
         setTimeout(function () {
+            // move the top icon to the left
             $('#current-icon')[0].style.transform = 'translate3d(-175%, 0, 0)';
         }, 500);
 
         var previous = previousCategory.innerHTML;
 
         setTimeout(function () {
+            // empty icon container at the top
             var top = $('.left .change-icon');
             top.empty();
 
+            // append previous icon top current option
             currentOption.children('.change-icon').append(previousIcon);
 
+            // calls the update function
             update(top, newIcon, previousCategory, newCategory);
+            // change the html value of the current option to the previous option
             currentOption.children('.category')[0].children[0].innerHTML = previous;
 
             setTimeout(function () {
+                // change opacity of previous category (show)
                 previousCategory.style.opacity = 1;
+                // change opacity of new category (show)
                 newCategory.style.opacity = 1;
             }, 250);
-            click[0].style.borderColor = previousColor;
-            click[0].style.opacity = '1';
-            click[0].style.borderWidth = '5px';
+            // change border color to previous icon color
+            currentOption[0].style.borderColor = previousColor;
+            // change opacity of option (show)
+            currentOption[0].style.opacity = '1';
+            // change left borderWidth of option (show)
+            currentOption[0].style.borderWidth = '10px';
         }, 1000);
     });
-
+    // FUNCTIONS
     function update(top, newIcon, previousCategory, newCategory) {
+        // calls animateTop function
         animateTop(newIcon, top);
+        // changes top category (previous) to new category chosen (new)
         previousCategory.innerHTML = newCategory.innerHTML;
     }
-
     function animateIcon() {
         setTimeout(function () {
             for (var i = 0; i < $('.option').length; i++) {
+                // moves the chosen categorie's icon to the right
                 $('.option .change-icon')[i].style.transform = 'translate3d(250%, 0, 0)';
                 setTimeout(function () {
+                    // moves the chosen categorie's icon back to position
                     $('#options .item')[0].lastElementChild.style.transform = 'translate3d(0, 0, 0)';
                 }, 500)
             }
         }, 500);
     }
-
+    
     function animateTop(newIcon, top) {
+        // append new icon
         top.append(newIcon);
+        // move icon to its original position
         $('#current-icon')[0].style.transform = 'translate3d(0, 0, 0)';
     }
 });
